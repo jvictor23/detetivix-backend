284de42021a1f0f671f469b1459d051a98ba76ee
Jvictor23 <joaovictor00788@hotmail.com>
2020-08-12 19:53:23 -0300

    ok

--- src/app/controller/missaoController.js
+++ src/app/controller/missaoController.js
@@ -2,61 +2,88 @@ const express = require('express');
 const Missao = require('../model/missao');
 const router = express.Router();
 const authMiddleware = require('../middlewares/auth');
+const User = require('../model/user');
 
 router.use(authMiddleware);
 
-router.post('/create', async (req,res)=>{
-   const missao = await Missao.create(req.body);
+router.post('/create', async (req, res) => {
+    try {
+        const verificaMissao = await Missao.findOne({ ativa: true });
 
-   res.send({missao});
+        if (verificaMissao) {
+            return res.send({ error: "Alguma missao ainda está ativa!" });
+        } else {
+            const missao = await Missao.create(req.body);
+
+            res.send({ missao });
+        }
+
+
+    } catch (error) {
+        return res.status(400).send({ error: "Não foi possivel criar a missão!" })
+    }
 });
 
-router.get('/authorization', async (req,res)=>{
-    res.send({ok: true});
+router.get('/authorization', async (req, res) => {
+    res.send({ ok: true });
 })
 
-router.post('/', async(req, res)=>{
-    const {idUser} = req.body;
+router.post('/', async (req, res) => {
+    const { idUser } = req.body;
     try {
-        const missao = await Missao.findOne({ativa:true, listUser: {$in: [idUser]}})
-        if(missao){
-            return res.send({EstadoMissao: "Concluida"});
-        }else{
-        const missao = await Missao.findOne({ativa: true});
-        return res.send(missao);
+        const missao = await Missao.findOne({ ativa: true, listUser: { $in: [idUser] } })
+        if (missao) {
+            return res.send({ EstadoMissao: "Concluida" });
+        } else {
+            const missao = await Missao.findOne({ ativa: true });
+            return res.send(missao);
         }
     } catch (error) {
-        return res.status(400).send({error: "Não foi possivel carregar a missao"});
+        return res.status(400).send({ error: "Não foi possivel carregar a missao" });
     }
 })
 
-router.post('/verify', async (req,res)=>{
+router.post('/verify', async (req, res) => {
 
-    const {resposta, idMissao, idUser} = req.body;
-    
+    const { resposta, idMissao, idUser, valorMissao } = req.body;
+
     try {
-        const  missao = await Missao.findById(idMissao).select("+resposta");
-        if(missao.resposta === resposta){
-            await Missao.findByIdAndUpdate(idMissao, {$addToSet:{listUser: idUser}})
-            return res.send({resposta: true})
-        }else{
-            return res.send({resposta: false});
+        const missao = await Missao.findOne({ ativa: true, listUser: { $in: [idUser] } })
+        if (missao) {
+            return res.send({ error: "Você já respondeu esta missão!" })
+        } else {
+
+            const missao = await Missao.findById(idMissao).select("+resposta");
+            if (missao.resposta === resposta) {
+                await Missao.findByIdAndUpdate(idMissao, { $addToSet: { listUser: idUser } })
+                const user = await User.findById(idUser);
+
+                const soma = parseInt(user.score) + parseInt(valorMissao);
+
+                await User.findByIdAndUpdate(idUser, {$set:{score: soma}});
+
+                return res.send({ resposta: true })
+            } else {
+                return res.send({ resposta: false });
+            }
+
         }
 
-        
+
+
     } catch (error) {
         console.log(error)
-        return res.status(400).send({error: "Não foi possivel verificar a resposta"});
+        return res.status(400).send({ error: "Não foi possivel verificar a resposta" });
     }
 
 })
 
-router.post('/close', async(req,res)=>{
+router.post('/close', async (req, res) => {
     try {
-        await Missao.findOneAndUpdate({ativa: true},{$set:{ativa: false}});
-        return res.send({ok: true});
+        await Missao.findOneAndUpdate({ ativa: true }, { $set: { ativa: false } });
+        return res.send({ ok: true });
     } catch (error) {
-        return res.status(400).send({error: "Não foi possivel fechar a missão atual"});
+        return res.status(400).send({ error: "Não foi possivel fechar a missão atual" });
     }
 })
 
