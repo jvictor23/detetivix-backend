4c8a743597b4920f87b11f480d02cb04156ec32b
Jvictor23 <joaovictor00788@hotmail.com>
2020-08-14 18:51:26 -0300

    desbilitando o cors

--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ const path = require('path');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-var corsOptions = {
-    origin: 'https://detetivix.com.br/',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-  }
-app.use(cors(corsOptions));
+// var corsOptions = {
+//     origin: 'https://detetivix.com.br/',
+//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+//   }
+app.use(cors());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp', 'uploads')))
 
 require('./app/controller/userController')(app);
